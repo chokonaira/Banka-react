bf0d177c80c8f3eafe6c73fa11a68efd0f056d83
Henry Okonkwo <okonkwo.henry2012@gmail.com>
2019-08-12 21:43:21 +0100

    feat(Banka):Banka notFound page
    Design a Not-Found page for banka with reactjs
    
    [Delivers #167855970]

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../styles/css/style.css';
+import '../styles/css/utils.css';
 
 
 const NotFoundPage = () => (
-  <div>
-    404 -
-    {' '}
-    <Link to="/">Go home</Link>
+  <div className="cont">
+    <div className="wrapper d-flex flex-col  a-i-center">
+      <h2 className="get-started1">PAGE NOT FOUND!</h2>
+      <Link to="/">Go home</Link>
+    </div>
+
   </div>
 );
 
