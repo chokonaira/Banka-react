de54b7f691c127b6474de389ddaa5780bb1ed6eb
Henry Okonkwo <okonkwo.henry2012@gmail.com>
2019-08-25 21:52:17 +0100

    feat(Banka): signup functionality
    setup redux for signup
    
    [#Deliver #168098084]

--- src/pages/SignupPage.js
+++ src/pages/SignupPage.js
@@ -1,78 +1,67 @@
-import React, { Component, createRef } from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
+import classnames from 'classnames';
 import PropTypes from 'prop-types';
 import { registerUser } from '../actions/authActions';
-import InputField from '../components/InputField';
+import Loader from '../components/Loader/index';
 import '../styles/css/style.css';
 import '../styles/css/utils.css';
 
 class signupForm extends Component {
-  state = {
-    firstname: '',
-    lastname: '',
-    email: '',
-    password: '',
-    errors: {},
-    mobile: false,
-  }
-
-  formRef = createRef()
-
-  componentDidMount() {
-    window.addEventListener('resize', this.handleResize);
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.errors) {
-      this.setState({
-        errors: nextProps.errors,
-      });
-    }
-  }
-
-  handleResize = () => {
-    const width = window.clientWidth;
-    const { mobile } = this.state;
-    if (width <= 1024 && !mobile) {
-      this.setState({ mobile: true });
-    } else if (width > 1024 && mobile) {
-      this.setState({ mobile: false });
-    }
-  }
-
-  changeValueHandler = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
-  }
-
-  onSubmit = (e) => {
-    e.preventDefault();
-    // console.log(this.formRef.current.className);
-    const newUserDetails = {
-      firstname: this.state.firstname,
-      lastname: this.state.lastname,
-      email: this.state.email,
-      password: this.state.password,
+  constructor() {
+    super();
+    this.state = {
+      firstname: '',
+      lastname: '',
+      email: '',
+      password: '',
     };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
 
-    this.props.registerUser(newUserDetails, this.props.history);
+  componentDidUpdate() {
+    const { register, history } = this.props;
+    if (register.isAuthenticated) {
+      history.push('/dashboard');
+    }
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+    const {
+      firstname, lastname, email, password,
+    } = this.state;
+
+    const userData = {
+      firstname,
+      lastname,
+      email,
+      password,
+    };
+    this.props.registerUser(userData);
+  }
+
+  onChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
   }
 
   render() {
-    const { errors } = this.state;
+    const {
+      firstname, lastname, email, password,
+    } = this.state;
+    const { errors, register } = this.props;
+    const { error } = errors;
+    const { loading } = register;
+    const errorLoading = errors.loading;
 
-    const labels = {
-      firstname: 'firstname:',
-      lastname: 'lastname:',
-      email: 'Email:',
-      password: 'Password:',
-    };
-
-    const formTypes = {
-      text: 'text',
-      email: 'email',
-      password: 'password',
-    };
+    if (loading && errorLoading) {
+      return (
+        <div>
+          <Loader />
+        </div>
+      );
+    }
 
     return (
       <div className="cont">
@@ -81,53 +70,71 @@ class signupForm extends Component {
             <h2 className="text-center">Sign Up</h2>
           </div>
           <form className="col-sm-5 main-form" onSubmit={this.onSubmit} ref={this.formRef} noValidate>
-            <div>
-              <InputField
-                classes={`form-control ${errors.firstname ? 'is-invalid' : ''}`}
-                type={formTypes.text}
-                label={labels.firstname}
-                value={this.state.firstname}
-                change={this.changeValueHandler}
-                name="firstname"
-                error={this.state.errors.firstname}
-              />
-            </div>
-            <div>
-              <InputField
-                classes={`form-control ${errors.lastname ? 'is-invalid' : ''}`}
-                type={formTypes.text}
-                label={labels.lastname}
-                value={this.state.lastname}
-                change={this.changeValueHandler}
-                name="lastname"
-                error={this.state.errors.lastname}
-              />
-            </div>
-
-            <div>
-              <InputField
-                classes={`form-control ${errors.email ? 'is-invalid' : ''}`}
-                type={formTypes.email}
-                label={labels.email}
-                value={this.state.email}
-                change={this.changeValueHandler}
-                name="email"
-                error={this.state.errors.email}
-              />
-            </div>
-
             <div className="form-group">
-              <InputField
-                classes={`form-control ${errors.password ? 'is-invalid' : ''}`}
-                type={formTypes.password}
-                label={labels.password}
-                value={this.state.password}
-                change={this.changeValueHandler}
-                name="password"
-                error={this.state.errors.password}
+              <p>Firstname:</p>
+              <input
+                type="firstname"
+                className={classnames('form-control form-control-lg', {
+                  'is-invalid': error.firstname,
+                })}
+                placeholder="Firstname"
+                name="firstname"
+                value={firstname}
+                onChange={this.onChange}
               />
+              {error.firstname && (
+              <div className="invalid-feedback">{error.firstname}</div>
+              )}
             </div>
-            <input type="submit" value="Register" id="submit" className="btn-default" />
+            <div className="form-group">
+              <p>Lastname:</p>
+              <input
+                type="lastname"
+                className={classnames('form-control form-control-lg', {
+                  'is-invalid': error.lastname,
+                })}
+                placeholder="Lastname"
+                name="lastname"
+                value={lastname}
+                onChange={this.onChange}
+              />
+              {error.lastname && (
+              <div className="invalid-feedback">{error.lastname}</div>
+              )}
+            </div>
+            <div className="form-group">
+              <p>Email:</p>
+              <input
+                type="email"
+                className={classnames('form-control form-control-lg', {
+                  'is-invalid': error.email,
+                })}
+                placeholder="Email Address"
+                name="email"
+                value={email}
+                onChange={this.onChange}
+              />
+              {error.email && (
+              <div className="invalid-feedback">{error.email}</div>
+              )}
+            </div>
+            <div className="form-group">
+              <p>Password:</p>
+              <input
+                type="password"
+                className={classnames('form-control form-control-lg', {
+                  'is-invalid': error.password,
+                })}
+                placeholder="Password"
+                name="password"
+                value={password}
+                onChange={this.onChange}
+              />
+              {error.password && (
+              <div className="invalid-feedback">{error.password}</div>
+              )}
+            </div>
+            <input type="submit" value="Submit" id="submit" className="btn-default" />
           </form>
         </div>
       </div>
@@ -136,20 +143,14 @@ class signupForm extends Component {
 }
 
 signupForm.propTypes = {
-  registerUser: PropTypes.func.isRequired,
-  auth: PropTypes.shape({
-    userName: PropTypes.string,
-    email: PropTypes.string,
-    password: PropTypes.string,
-  }).isRequired,
-  errors: PropTypes.shape({}).isRequired,
-  history: PropTypes.shape({}).isRequired,
+  registerUser: PropTypes.func,
+  register: PropTypes.object,
+  errors: PropTypes.object,
 };
 
-
 const mapStateToProps = state => ({
-  auth: state.auth,
+  register: state.auth,
   errors: state.errors,
 });
 
-export default connect(mapStateToProps, { registerUser })(withRouter(signupForm));
+export default connect(mapStateToProps, { registerUser })(signupForm);
