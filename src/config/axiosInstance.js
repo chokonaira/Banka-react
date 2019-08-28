5e13ea05212e0b22c288dff23362574128e9ef4e
Henry Okonkwo <okonkwo.henry2012@gmail.com>
2019-08-28 01:42:35 +0100

    feat(Bank):dashboard functionality
    Implement user dashboard functionality
    
    [Deliver #168144352]

--- src/config/axiosInstance.js
+++ src/config/axiosInstance.js
@@ -1,7 +1,14 @@
 import axios from 'axios';
 
-const instance = axios.create({
-  baseURL: 'https://banka101.herokuapp.com/api/v1',
-});
+const axiosWithAuth = () => {
+  const token = localStorage.getItem('jwtToken');
+  return axios.create({
+    baseURL: 'https://banka101.herokuapp.com/api/v1',
+    headers: {
+      'Content-Type': 'application/json',
+      token,
+    },
+  });
+};
 
-export default instance;
+export default axiosWithAuth;
--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import Dashboard from '../components/Dashboard';
+import Dashboard from '../components/DashBoard';
 
 
 const DashboardPage = () => (
